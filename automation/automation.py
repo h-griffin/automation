522fdd76cc16bffcdaebbabb4a2f9ffb1acfefee
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-04 13:39:22 -0700

    lecture fake

--- automation/automation.py
+++ automation/automation.py
@@ -9,4 +9,16 @@ print(fake.email())
 print(fake.phone_number())
 
 
+content = ''
+for _ in range(10): #one argument = stop at
+    content += fake.paragraph()
+    content += fake.email()
+    content += fake.paragraph()
+    content += fake.phone_number()
+    content += fake.paragraph()
+content+= '\n'
+print(content)
 
+
+with open('notes.txt', 'w+') as f:
+    f.write(content)
