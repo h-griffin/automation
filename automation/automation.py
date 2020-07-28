0d3168f69c7369e3aee73489762cdd841e4c7bc6
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-28 14:40:17 -0700

    comments

--- automation/automation.py
+++ automation/automation.py
@@ -11,21 +11,21 @@
 import re
 
 def pull_numbers(original, new):
-    """takes in 2 files as arguments, one origional file to read from and a new file to print phone numbers from origional"""
+    """takes in 2 files as arguments, one original file to read from and a new file to print phone numbers from original"""
     with open(original) as contents:
         contents = contents.read()
 
-        ten_digits = re.findall(r'\d{3}.\d{3}.\d{4}', contents)
-        erase_paren_dot = [num.replace(')', "-") for num in ten_digits]
-        erase_dot = [num.replace('.', '-') for num in erase_paren_dot]
-        erase_dot.sort()
+        ten_digits = re.findall(r'\d{3}.\d{3}.\d{4}', contents)         # find any format phone number
+        erase_paren_dot = [num.replace(')', "-") for num in ten_digits] # replace parens with dash
+        erase_dot = [num.replace('.', '-') for num in erase_paren_dot]  # replace dot with dash
+        erase_dot.sort()                                                # sort numbers in order
 
-    with open(new, 'w+') as f:
+    with open(new, 'w+') as f:      # w+ write to existing, or create if not existing
         for num in erase_dot:
-            f.write(num + '\n')
+            f.write(num + '\n')     # write to file
 
 def pull_emails(original, new):
-    """takes in 2 files as arguments, one origional file to read from and a new file to print emails from origional"""
+    """takes in 2 files as arguments, one original file to read from and a new file to print emails from original"""
     with open(original) as contents:
         contents = contents.read()
 
